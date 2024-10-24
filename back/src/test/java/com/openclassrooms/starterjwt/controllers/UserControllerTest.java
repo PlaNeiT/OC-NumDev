d95eeb4980dd54a99c5ca752ee66758d5ad76b24
leop <leopetit200@gmail.com>
2024-10-24 20:21:10 +0200

    .

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
@@ -0,0 +1,71 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String obtainAccessToken() throws Exception {
+        LoginRequest loginRequest = createLoginRequest("yoga@studio.com", "test!1234");
+        String loginRequestJson = objectMapper.writeValueAsString(loginRequest);
+
+        MvcResult result = mockMvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginRequestJson))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        return JsonPath.read(response, "$.token");
+    }
+
+    private LoginRequest createLoginRequest(String email, String password) {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail(email);
+        loginRequest.setPassword(password);
+        return loginRequest;
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+        String token = obtainAccessToken();
+
+        mockMvc.perform(get("/api/user/1")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.email").value("yoga@studio.com"))
+                .andExpect(jsonPath("$.admin").value(true));
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception {
+        String token = obtainAccessToken();
+
+        mockMvc.perform(delete("/api/user/1")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
